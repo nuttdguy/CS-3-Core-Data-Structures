5a95345a8b4c0893f0b9daa9e35bbcc196225797
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-10-24 23:31:33 -0700

    base 2 to 10

--- source/bases.py
+++ source/bases.py
@@ -18,13 +18,22 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # TODO: Decode digits from binary (base 2)
-    # ...
+    if base is 10:
+        result = 0
+        power = 0
+        while digits > 0:
+            pw = 2 ** power
+            mod = digits % 10
+            result += pw * mod
+            digits = round(digits / 10)
+            power += 1
+        return result
     # TODO: Decode digits from hexadecimal (base 16)
     # ...
     # TODO: Decode digits from any base (2 up to 36)
     # ...
 
-
+    # 128 | 64 | 32 | 16 >> 8 | 4 | 2 | 0
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
     number: int -- integer representation of number (in base 10)
@@ -34,11 +43,24 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
+    # TODO: Encode number in binary (base 2) [1010 1010 1010] (12)
+    if base is 2:
+        binary = 0
+        power = 0
+        while number > 0:
+            pw = 10 ** power
+            mod = number % 2
+            binary += pw * mod
+            number = round(number / 2)
+            power += 1
+        return binary
+    # TODO: Encode number in hexadecimal (base 16) [9AEF 0B1C 001A 4512]
+    if base is 16:
+        hex_digits = ['A', 'B', 'C', 'D', 'E', 'F']
+
+
+
+    # TODO: Encode number in any base (2 up to 36)  [??]
     # ...
 
 
@@ -52,13 +74,18 @@ def convert(digits, base1, base2):
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
     # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
+    # if digits is string.digits:
+        # BASE II IS BINARY (ENCODE) e.g. '1010 1010 1010 1011 1010 1010 1010 1011'
+    print('base1: {}  == base2: {}  == digits(): {}'.format(base1, base2, digits))
+    return decode(digits, base1)
     # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
+        # BASE II IS BINARY (ENCODE TO 10)
+
     # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
+        # BASE 10 TO BASE 16 (ENCODE FROM 2 10 16)
+
     # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+        # REVISIT AFTER ALL OTHER IMPLEMENTATIONS
 
 
 def main():
@@ -76,6 +103,13 @@ def main():
         print('Usage: {} digits base1 base2'.format(sys.argv[0]))
         print('Converts digits from base1 to base2')
 
+# string.digits is '0123456789'
+# string.hexdigits is '0123456789abcdefABCDEF'
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+# string.printable is digits + ascii_letters + punctuation + whitespace
 
 if __name__ == '__main__':
     main()
+
