ae33cef285e881ca7ba15a7b594008632ad2d19e
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-11-08 21:27:35 -0800

    LinkedStack

--- source/stack.py
+++ source/stack.py
@@ -21,27 +21,41 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        if self.list.is_empty():
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        # Count number of items
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        # Push given item
+        return self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        # Return top item, if any
+        if self.length() is not 0:
+            item = self.list.tail
+            return item.data
+        return None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        # Remove and return top item, if any
+        if self.list.is_empty():
+            raise ValueError
+        item = self.peek()
+        self.list.delete(item)
+        return item
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
